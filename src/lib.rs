8655439fae77af8702884d03283dc8272d02e330
Wybe Westra <dev@wwestra.nl>
2022-12-14 09:52:24 +0100

    feat: Added leafwing input plugin.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
 use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
+use tracing::info;
 
 pub struct MainPlugin;
 
 impl Plugin for MainPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_plugin(InputManagerPlugin::<Action>::default())
+            .add_startup_system(spawn_player)
+            .add_system(hello_system);
+    }
+}
+
+#[derive(Component)]
+struct Player;
+
+fn spawn_player(mut commands: Commands) {
+    commands
+        .spawn(InputManagerBundle::<Action> {
+            action_state: ActionState::default(),
+            input_map: InputMap::new([(KeyCode::Space, Action::HelloAction)]),
+        })
+        .insert(Player);
+}
+
+fn hello_system(query: Query<&ActionState<Action>, With<Player>>) {
+    let action_state = query.single();
+
+    if action_state.just_pressed(Action::HelloAction) {
+        info!("Hello World!");
+    }
+}
+
+#[derive(Actionlike, Eq, PartialEq, Clone, Copy, Hash, Debug)]
+enum Action {
+    HelloAction,
 }
