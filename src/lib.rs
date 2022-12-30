f24378b7fd305a588ab4f72ddc1c6d8b68c18655
Wybe Westra <dev@wwestra.nl>
2022-12-30 19:37:46 +0100

    feat: Can select and deselect planets.

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ use bevy::log::LogPlugin;
 use bevy::math::Vec3Swizzles;
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
 use tracing::info;
 
 const GREETING_ID: &str = "greeting";
@@ -43,6 +44,7 @@ impl Plugin for MainPlugin {
         app.add_plugin(InputPlugin)
             .add_plugin(LocalizationPlugin)
             .add_plugin(GameCameraPlugin)
+            .add_plugin(EguiPlugin)
             .add_loading_state(
                 LoadingState::new(GameState::AssetLoading)
                     .continue_to_state(GameState::Main)
@@ -57,7 +59,8 @@ impl Plugin for MainPlugin {
             .add_system_set(
                 SystemSet::on_update(GameState::Main)
                     .with_system(greeting_system)
-                    .with_system(planet_selection_system),
+                    .with_system(planet_selection_system)
+                    .with_system(planet_info_ui),
             );
     }
 }
@@ -75,17 +78,24 @@ enum GameState {
 }
 
 #[derive(Component)]
-struct Planet;
+struct Planet {
+    name: String,
+}
+
+#[derive(Component)]
+struct Selected;
 
 fn spawn_planets(mut commands: Commands, image_assets: Res<ImageAssets>) {
-    for i in 0..10 {
+    for index in 0..10 {
         commands
             .spawn(SpriteBundle {
                 texture: image_assets.planet.clone(),
-                transform: Transform::from_xyz(i as f32 * 100.0, 0.0, 0.0),
+                transform: Transform::from_xyz(index as f32 * 100.0, 0.0, 0.0),
                 ..default()
             })
-            .insert(Planet);
+            .insert(Planet {
+                name: format!("Planet {index}"),
+            });
     }
 }
 
@@ -93,16 +103,46 @@ fn planet_selection_system(
     mut commands: Commands,
     actions: ActionRes,
     cursor_position: Res<GlobalCursorPosition>,
-    planets: Query<&GlobalTransform, With<Planet>>,
+    planets: Query<(Entity, &GlobalTransform, Option<&Selected>), With<Planet>>,
 ) {
     if actions.just_pressed(Action::PrimaryInteraction) {
-        for planet in planets.iter() {
+        let mut found_planet = false;
+
+        for (entity, transform, maybe_selected) in planets.iter() {
             // TODO (Wybe 2022-12-30): Put in planet sizes.
-            if cursor_position.distance(planet.translation().xy()) < 10.0 {
-                println!("Clicked a planet!");
+            if cursor_position.distance(transform.translation().xy()) < 16.0 {
+                found_planet = true;
+
+                if maybe_selected.is_none() {
+                    commands.entity(entity).insert(Selected);
+                } else {
+                    commands.entity(entity).remove::<Selected>();
+                }
+            }
+        }
+
+        if !found_planet {
+            // No planets found? Deselect all.
+            for (entity, _, maybe_selected) in planets.iter() {
+                if maybe_selected.is_some() {
+                    commands.entity(entity).remove::<Selected>();
+                }
             }
         }
     }
+}
+
+fn planet_info_ui(
+    mut egui_context: ResMut<EguiContext>,
+    selected_planets: Query<&Planet, With<Selected>>,
+) {
+    egui::Window::new("Selected planets")
+        .anchor(egui::Align2::LEFT_TOP, [0., 0.])
+        .show(egui_context.ctx_mut(), |ui| {
+            for planet in selected_planets.iter() {
+                ui.label(&planet.name);
+            }
+        });
 }
 
 fn greeting_system(actions: ActionRes, localization: Res<Localization>) {
